a00ea2d353da3341fde936972f5ad58bfaecaf92
Oleksand Tkach <sashatkach66@gmail.com>
2024-11-10 19:33:07 +0200

    message commit

--- JS/task-2.js
+++ JS/task-2.js
@@ -1,8 +1,10 @@
-function getShippingMessage(country, price, deliveryFee ){
-    const message2 =`Shipping to ${country} will cost ${totalPrice} credits`;
-    return message2;
- 
+function getShippingMessage(country, price, deliveryFee) {
+  const message2 = `Shipping to ${country} will cost ${totalPrice} credits`;
+  return message2;
 }
 onsole.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
+console.log(message2);
 console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits" 
+console.log(message2);
+console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+console.log(message2);
--- JS/task-3.js
+++ JS/task-3.js
@@ -1,8 +1,10 @@
-function getShippingMessage(content, padding, border ){
-    const width =content+(padding+border)*2; 
-    return message3;
- 
+function getShippingMessage(content, padding, border) {
+  const width = Number.parseFloat(content + (padding + border) * 2);
+  return width;
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(width);
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(width);
 console.log(getElementWidth("200px", "0px", "0px")); //200
+console.log(width);
